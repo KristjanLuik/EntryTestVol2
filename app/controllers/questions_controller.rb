22a77c5788f7bdbc31d20893dda3d896ad551250
tiiatanav <ttanav@itcollege.ee>
2014-04-30 15:17:02 +0300

    answers can be added/deleted trough questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -15,6 +15,7 @@ class QuestionsController < ApplicationController
   # GET /questions/new
   def new
     @question = Question.new
+    @question.answers.build
   end
 
   # GET /questions/1/edit
@@ -69,6 +70,6 @@ class QuestionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def question_params
-      params.require(:question).permit(:content, :difficulty, :status)
+      params.require(:question).permit(:content, :difficulty, :status, answers_attributes: [:id, :content, :points, :correct, :_destroy])
     end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,13 @@
 class Question < ActiveRecord::Base
+	# connections 
 	has_many :answers
 	has_many :labels, :through => :labels_questions
 	has_many :labels_questions
+	
+	# attributes 
+	accepts_nested_attributes_for :labels, reject_if:  :all_blank, allow_destroy: true
+	accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true #http://guides.rubyonrails.org/form_helpers.html#configuring-the-model
+
+	# validations
+	
 end
