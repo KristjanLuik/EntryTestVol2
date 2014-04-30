156a0b5815996810a0d6a1d48e65bf7c8bdc7c25
tiiatanav <ttanav@itcollege.ee>
2014-04-30 16:16:11 +0300

    basic label adding to questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -16,10 +16,13 @@ class QuestionsController < ApplicationController
   def new
     @question = Question.new
     @question.answers.build
+    @question.labels.build
   end
 
   # GET /questions/1/edit
   def edit
+    @question.answers.build
+    @question.labels.build
   end
 
   # POST /questions
@@ -70,6 +73,6 @@ class QuestionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def question_params
-      params.require(:question).permit(:content, :difficulty, :status, answers_attributes: [:id, :content, :points, :correct, :_destroy])
+      params.require(:question).permit(:content, :difficulty, :status, answers_attributes: [:id, :content, :points, :correct, :_destroy], labels_attributes: [:id, :name])
     end
 end
