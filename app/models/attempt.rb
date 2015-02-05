5d93bf8b7062514f0149b3a2bbbc663a40170844
tiiatanav <ttanav@itcollege.ee>
2015-02-05 15:52:03 +0200

    erd added

--- app/models/attempt.rb
+++ app/models/attempt.rb
@@ -12,32 +12,9 @@ def generate_test
 	# hangi test
 	test=self.timetable.test
 	# vali testiga seotud kysimused (seos on l2bi labelite?) TODO!
-
 	self.add_by_difficulty('kerge', test.easy_count)
 	self.add_by_difficulty('keskmine', test.medium_count)
-	self.add_by_difficulty('raske', test.hard_count)		
-	# questions = Question.order("RANDOM()").all # ajutine!
-	# # salvesta kysimused attempti kylge
-	# questions.each do |q|
-	# 	aq = AttemptQuestion.create(:question=>q, :attempt=>self)
-	# 	# vali salvestatud kysimuse vastused
-	# 	a_count=rand(q.min_options..q.max_options)
-	# 	c_count=rand(q.min_correct..q.max_correct)
-	# 	f_count=a_count-c_count
-	# 	logger.debug "\nthere are #{c_count} correct and #{f_count} incorrect answers \n"
-	# 	c = Answer.where(:correct=>true, :question=> q).order("RANDOM()").first(c_count)
-	# 	f = Answer.where(:correct=>false, :question=> q).order("RANDOM()").first(f_count)
-	# 	answers= c + f
-	# 	logger.debug "\ngot #{c.count} correct and #{f.count} incorrect answers totaling at #{answers.count} (before shuffle)\n"
-	# 	answers.shuffle!
-	# 	logger.debug "This question has a total of #{answers.count} answers\n"
-	# 	# salvesta vastused attempti kylge
-	# 	answers.each do |a|
-	# 		aa=AttemptAnswer.create(:answer=>a, :attempt=>self)
-	# 	end
-	# end
-
-	
+	self.add_by_difficulty('raske', test.hard_count)			
 end
 
 def add_by_difficulty(level, count)
